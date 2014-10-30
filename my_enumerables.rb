60144509c88e98c6220b34f3f9345a7e80848c59
Jeremy A Kansas <ymerejsasnak@gmail.com>
2014-10-30 15:22:08 -0400

    my_each works, but does not return what each returns with no block

--- my_enumerables.rb
+++ my_enumerables.rb
@@ -1,7 +1,15 @@
 module Enumerable
 
 	def my_each
-		
+
+    return self unless block_given?
+    counter = 0
+    0.upto(self.size - 1) do
+      
+    	yield(self[counter])
+      counter += 1
+    
+    end
 
 	end
 
