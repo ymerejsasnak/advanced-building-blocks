fa82a1283a621d5bed6eec662519a20cd5cae802
Jeremy A Kansas <ymerejsasnak@gmail.com>
2014-10-31 10:08:12 -0400

    my_select done.  trying to get all 3 working with hashes too

--- my_enumerables.rb
+++ my_enumerables.rb
@@ -5,8 +5,8 @@ module Enumerable
 
     return self.to_enum(:my_each) unless block_given?
 
-    for i in 0..(self.size - 1) do
-      yield self[i]
+    for i in 0..(self.to_a.size - 1) do
+      yield self.to_a[i]
     end
 
     return self
@@ -19,11 +19,31 @@ module Enumerable
     
     return self.to_enum(:my_each_with_index) unless block_given?
 
-    for i in 0..(self.size - 1) do
-    	yield self[i], i
+    for i in 0..(self.to_a.size - 1) do
+    	yield self.to_a[i], i
     end
 
     return self
+
+  end
+
+
+
+  def my_select
+
+  	return self.to_enum(:my_select) unless block_given?
+
+    result = []
+
+    for i in 0..(self.size - 1) do
+
+    	if yield self[i]
+    	  result << self[i]
+    	end
+
+    end
+    
+    return result
 
   end
 
