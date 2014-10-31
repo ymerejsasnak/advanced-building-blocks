1e97b594b803431c0effa007f4acbe79f5b6fd58
Jeremy A Kansas <ymerejsasnak@gmail.com>
2014-10-31 09:55:01 -0400

    my_each_with_index done!

--- my_enumerables.rb
+++ my_enumerables.rb
@@ -3,18 +3,27 @@ module Enumerable
 
 	def my_each
 
-    return self.to_enum unless block_given?
+    return self.to_enum(:my_each) unless block_given?
 
-    for i in 0..(self.length - 1) do
+    for i in 0..(self.size - 1) do
       yield self[i]
     end
+
+    return self
 
 	end
 
 
 
   def my_each_with_index
+    
+    return self.to_enum(:my_each_with_index) unless block_given?
 
+    for i in 0..(self.size - 1) do
+    	yield self[i], i
+    end
+
+    return self
 
   end
 
